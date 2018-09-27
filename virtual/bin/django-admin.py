dc35997e3346559a23d35d922b04dc2f74391906
EugeneZnm <eugenenzioki@gmail.com>
2018-09-27 15:11:33 +0300

    creation of project

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/eugene/Documents/Moringa/CORE/PYTHON/DJANGO/UNSPLASH/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
