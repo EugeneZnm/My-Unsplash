1793cfe04f696b60714ffc346d36c159a76213d7
EugeneZnm <eugenenzioki@gmail.com>
2018-09-27 15:25:00 +0300

    addition of name and descirption columns

--- myunsplash/models.py
+++ myunsplash/models.py
@@ -6,4 +6,5 @@ class Image(models.Model):
     """
     Image class creating table for Image
     """
-    Name =
+    Name = models.CharField(max_length=20)
+    Deascription = models.CharField(max_length=40)
