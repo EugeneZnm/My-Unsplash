4a8ba4a9e4566ad0cae7a882b29f410eec81ea20
EugeneZnm <eugenenzioki@gmail.com>
2018-09-27 15:57:32 +0300

    method to search for image by name

--- myunsplash/models.py
+++ myunsplash/models.py
@@ -6,5 +6,22 @@ class Image(models.Model):
     """
     Image class creating table for Image
     """
+    id = models.IntegerField()
     Name = models.CharField(max_length=20)
-    Deascription = models.CharField(max_length=40)
+    Description = models.CharField(max_length=40)
+
+    def save_image(self):
+        """
+        method to save image and its details
+        :return:
+        """
+        self.save()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        """
+        method to search for image
+        :return:
+        """
+        image = cls.object.filter(name__contains=search_term)
+
