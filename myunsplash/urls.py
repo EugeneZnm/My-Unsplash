5a827486d182b0e4fc11e5678a4abf562aea3702
EugeneZnm <eugenenzioki@gmail.com>
2018-09-28 18:23:19 +0300

    adding search form in navbar

--- myunsplash/urls.py
+++ myunsplash/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.Home,name='Home'),
     url('^single-image/(\d+)',views.single_image,name='One-Pic'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
