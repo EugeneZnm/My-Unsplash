8fbca5b5344cec087e9d6d49d1d7fe3a29141675
EugeneZnm <eugenenzioki@gmail.com>
2018-09-28 11:41:48 +0300

    defining app and bootstrap

--- heyapp/settings.py
+++ heyapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myunsplash.apps.MyunsplashConfig',
+    'bootstrap4',
+
 ]
 
 MIDDLEWARE = [
--- myunsplash/models.py
+++ myunsplash/models.py
@@ -2,6 +2,35 @@ from django.db import models
 
 
 # Create your models here.
+
+
+class Location(models.Model):
+    """
+    class to save location of where the photograph was taken
+    """
+    locale = models.CharField(max_length=30)
+
+    def save_location(self):
+        """
+        method to save location
+        :return:
+        """
+        self.save()
+
+    def __str__(self):
+        return self.locale
+
+
+class Category(models.Model):
+    """
+    category model to save image categories
+    """
+    category = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.category
+
+
 class Image(models.Model):
     """
     Image class creating table for Image
@@ -9,8 +38,7 @@ class Image(models.Model):
     Name = models.CharField(max_length=20)
     Description = models.CharField(max_length=40)
     image =  models.ImageField(upload_to='images/', blank=True)
-    location = models.ForeignKey(location)
-    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
     Category = models.ManyToManyField(Category)
 
     def save_image(self):
@@ -56,28 +84,4 @@ class Image(models.Model):
         return images
 
 
-class Location(models.Model):
-    """
-    class to save location of where the photograph was taken
-    """
-    locale = models.CharField(max_length=20)
 
-    def save_location(self):
-        """
-        method to save location
-        :return:
-        """
-        self.save()
-
-    def __str__(self):
-        return self.locale
-
-
-class Category(models.Model):
-    """
-    category model to save image categories
-    """
-    category = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.category
