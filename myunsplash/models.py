5b7c8ec3627b3e7fb1042223462a94daed4c9865
EugeneZnm <eugenenzioki@gmail.com>
2018-09-29 19:33:39 +0300

    styling

--- myunsplash/models.py
+++ myunsplash/models.py
@@ -8,7 +8,7 @@ class Location(models.Model):
     """
     class to save location of where the photograph was taken
     """
-    locale = models.CharField(max_length=30)
+    locale = models.CharField(max_length=300)
 
     def save_location(self):
         """
@@ -25,7 +25,7 @@ class Category(models.Model):
     """
     category model to save image categories
     """
-    category = models.CharField(max_length=10)
+    category = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.category
@@ -35,8 +35,9 @@ class Image(models.Model):
     """
     Image class creating table for Image
     """
-    Name = models.CharField(max_length=20)
-    Description = models.CharField(max_length=40)
+    Name = models.CharField(max_length=200)
+    Description = models.CharField(max_length=4000)
+    By = models.CharField(max_length=1000, default=True)
     image = models.ImageField(upload_to='images/', blank=True)
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category, default=True)
